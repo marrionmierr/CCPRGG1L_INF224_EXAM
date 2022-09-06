151bdd307af3c186d0450f590c6154639feb26f6
marrionmierr <111546913+marrionmierr@users.noreply.github.com>
2022-09-06 09:50:53 +0800

    Create App.java

---
+++ exam-1/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student Mierr = new Student();
+
+        Mierr.surname = "Mierr";
+        Mierr.firstName = "Marrion Chino";
+        Mierr.middleInitial = "B";
+        Mierr.dateOfBirth = "March 26, 2004";
+        Mierr.studentNumber = 2022100836;
+        Mierr.studentEmailAddress = "mierrmb@students.national-u.edu.ph";
+        Mierr.iAmAwesome = true;
+        Mierr.sayMystudentNumber();
+        Mierr.sayMystudentEmailAddress();
+        Mierr.sayMyiAmAwesome();
+    }
+    
+}
